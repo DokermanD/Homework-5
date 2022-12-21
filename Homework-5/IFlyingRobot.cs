98671f02158e520124032089d689931acdf537a4
Дмитрий Хаткевич <dmitry.hatkevich@yandex.ru>
2022-12-22 01:38:02 +0300

    Добавьте файлы проекта.

---
+++ Homework-5/IFlyingRobot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_5
+{
+    internal interface IFlyingRobot : IRobot
+    {
+        new string GetRobotType()
+        { 
+            return "I am a flying robot.";
+        }
+    }
+}
---
+++ Homework-5/IRobot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_5
+{
+    internal interface IRobot
+    {
+        public string GetInfo();
+        public List<string> GetComponents();
+        public string GetRobotType()
+        {
+            return "I am a simple robot.";
+        }
+    }
+}
